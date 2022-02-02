be41e6109c53200a51ee2efc1e65c22bd33f346a
Guillaume Pujol <guillaume.pujol@axa.com>
2022-02-02 10:51:03 +0100

    doc formatting

--- binapy/binapy.py
+++ binapy/binapy.py
@@ -23,10 +23,12 @@ except ImportError:
 
 class BinaPy(bytes):
     """
-    This is the only class that you will use to manipulate binary data.
-    Since this is a bytes subclass, you can use instances of BinaPy anywhere you can use bytes.
-    This provides a few helper methods compared to plain bytes.
-    Actual transformations such as `encode_b64()` or `hash_sha256()` are implemented using Extensions.
+    This subclass of `bytes` exposes various binary data manipulation methods.
+    Since this is a `bytes` subclass, you can use instances of `BinaPy` anywhere you can use `bytes`.
+    BinaPy allows (re)encoding of data using `encode_to(<format>)`, decoding using `decode_from(<format>)`,
+    parsing using `parse_from(<format>)`, and serialisation using `serialize_to(<format>)`.
+
+    Actual transformations into formats such as Base64, JSON, etc. are implemented using Extensions.
     Those extensions are registered using the decorators `binapy_encoder`, `binapy_decoder`, `binapy_checker`,
     `binapy_serializer`, and `binapy_parser`.
     """
@@ -39,9 +41,11 @@ class BinaPy(bytes):
     ) -> "BinaPy":
         """
         Overrides base method to accept a string with a default encoding of "utf-8".
+
         See Also:
             [`bytes` constructor](https://docs.python.org/3/library/stdtypes.html#bytes) and
             [`str.encode()`](https://docs.python.org/3/library/stdtypes.html#str.encode)
+
         Args:
             value: a `bytes` or a `str`
             encoding: if value is a `str`, specifies the encoding to use to encode this str to bytes
@@ -62,9 +66,11 @@ class BinaPy(bytes):
         signed: bool = False,
     ) -> "BinaPy":
         """
-        Convert an `int` to a `BinaPy`. This is a wrapper around
-        [int.to_bytes()](https://docs.python.org/3/library/stdtypes.html#int.to_bytes) and takes
-        the same parameters.
+        Convert an `int` to a `BinaPy`.
+
+        This is a wrapper around [int.to_bytes()](https://docs.python.org/3/library/stdtypes.html#int.to_bytes) and
+        takes the same parameters.
+
         Args:
             i: the integer to convert to BinaPy
             length: the length of the integer, in bytes. If omitted, takes the minimal length that fits the given integer.
@@ -84,6 +90,7 @@ class BinaPy(bytes):
     def ascii(self) -> str:
         """
         Convert this BinaPy to a str, making sure that only ascii characters are employed.
+
         Returns: a str
         """
         return self.decode("ascii")
@@ -97,6 +104,7 @@ class BinaPy(bytes):
         This is a wrapper around
         [int.from_bytes()](https://docs.python.org/3/library/stdtypes.html#int.from_bytes) and takes
         the same parameters.
+
         Args:
             byteorder: "little" or "big" (defaults to "big")
             signed: determines whether two’s complement is used to represent the integer. Default to False.
@@ -134,6 +142,7 @@ class BinaPy(bytes):
         Args:
             byteorder: byte order to use
             signed: True if 2 complement is used to represent negative values
+
         Returns:
             a string with containing only 0 and 1
         """
@@ -143,6 +152,7 @@ class BinaPy(bytes):
     def random(cls, length: int) -> "BinaPy":
         """
         Return a BinaPy containing `length` random bytes
+
         Args:
             length: number of bytes to generate
 
@@ -177,6 +187,7 @@ class BinaPy(bytes):
     def __getitem__(self, slice: Union[int, slice]) -> "Union[int, BinaPy]":
         """
         Override the base method so that slicing returns a BinaPy instead of just bytes.
+
         Args:
             index: an index
 
@@ -192,6 +203,7 @@ class BinaPy(bytes):
         Return the character at the given index.
 
         Slicing a standard bytes returns an int. Sometimes what you really want is a single char string.
+
         Args:
             index:
 
@@ -203,6 +215,7 @@ class BinaPy(bytes):
     def __add__(self, other: bytes) -> "BinaPy":
         """
         Override base method so that addition returns a BinaPy instead of bytes.
+
         Args:
             other: bytes or BinaPy to add
 
@@ -214,6 +227,7 @@ class BinaPy(bytes):
     def __radd__(self, other: bytes) -> "BinaPy":
         """
         Override base method so that right addition returns a BinaPy instead of bytes.
+
         Args:
             other: bytes or BinaPy to radd
 
@@ -225,6 +239,7 @@ class BinaPy(bytes):
     def split(self, sep: Optional[bytes] = None, maxsplit: int = -1) -> "List[BinaPy]":  # type: ignore[override]
         """
         Override base method so that split() returns a BinaPy instead of bytes.
+
         Args:
             sep: a separator
             maxsplit: the maximum number of splits
@@ -237,6 +252,7 @@ class BinaPy(bytes):
     def cut_at(self, *pos: int) -> "List[BinaPy]":
         """
         Cut this BinaPy at one or more integer positions.
+
         Args:
             *pos: indexes where to cut the BinaPy
 
@@ -309,7 +325,8 @@ class BinaPy(bytes):
 
     def encode_to(self, name: str, *args: Any, **kwargs: Any) -> "BinaPy":
         """
-        Encodes data from this BinaPy according to the extension format `name`.
+        Encode data from this BinaPy according to the extension format `name`.
+
         Args:
             name: extension name to use
             *args: additional position parameters for the extension encoder method
@@ -324,7 +341,8 @@ class BinaPy(bytes):
 
     def decode_from(self, name: str, *args: Any, **kwargs: Any) -> "BinaPy":
         """
-        Decodes data from this BinaPy according to the format `name`.
+        Decode data from this BinaPy according to the format `name`.
+
         Args:
             name: extension name to use
             *args: additional position parameters for the extension decoder method
@@ -341,7 +359,8 @@ class BinaPy(bytes):
         self, name: str, decode: bool = False, raise_on_error: bool = False
     ) -> bool:
         """
-        Checks that this BinaPy conforms to a given extension format.
+        Check that this BinaPy conforms to a given extension format.
+
         Args:
             name: the name of the extension to check
             decode: if True, and the given extension doesn't have a checker method, try to decode this BinaPy using the decoder method  to check if that works.
@@ -380,7 +399,8 @@ class BinaPy(bytes):
 
     def check_all(self, decode: bool = False) -> List[str]:
         """
-        Checks if this BinaPy conforms to any of the registered extensions.
+        Check if this BinaPy conforms to any of the registered extensions.
+
         Returns: a list of extensions that this BinaPy can be decoded from.
 
         Args:
@@ -398,6 +418,7 @@ class BinaPy(bytes):
     def parse_from(self, name: str, *args: Any, **kwargs: Any) -> Any:
         """
         Parse data from this BinaPy, based on a given extension format.
+
         Args:
             name: name of the extension to use
             *args: additional position parameters for the extension decoder method
@@ -414,6 +435,7 @@ class BinaPy(bytes):
     def serialize_to(cls, name: str, *args: Any, **kwargs: Any) -> "BinaPy":
         """
         Serialize (dump) data to a BinaPy, based on a given extension format.
+
         Args:
             name: name of the extension to use
             *args: additional position parameters for the extension decoder method (which includes the data to serialize)
