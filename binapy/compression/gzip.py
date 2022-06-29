6b34e62620544320d52880bf9d119207604e1004
Guillaume Pujol <guillaume.pujol@axa.com>
2022-06-29 09:44:55 +0200

    added gzip options, added SAML requests compression/decompression

--- binapy/compression/gzip.py
+++ binapy/compression/gzip.py
@@ -9,5 +9,17 @@ def compress_gzip(bp: bytes, level: int = -1) -> bytes:
 
 
 @binapy_decoder("gzip")
-def decompress_gzip(bp: bytes) -> bytes:
-    return zlib.decompress(bp)
+def decompress_gzip(
+    bp: bytes, wbits: int = zlib.MAX_WBITS, bufsize: int = zlib.DEF_BUF_SIZE
+) -> bytes:
+    return zlib.decompress(bp, wbits=wbits, bufsize=bufsize)
+
+
+@binapy_encoder("saml")
+def compress_saml(bp: bytes, level: int = -1) -> bytes:
+    return zlib.compress(bp, level=level)[2:-4]
+
+
+@binapy_decoder("saml")
+def decompress_saml(bp: bytes, bufsize: int = zlib.DEF_BUF_SIZE) -> bytes:
+    return zlib.decompress(bp, wbits=-15, bufsize=bufsize)
---
+++ tests/test_compression.py
@@ -0,0 +1,33 @@
+from binapy import BinaPy
+
+
+def test_gzip() -> None:
+    assert (
+        BinaPy("this is a test").to("gzip", level=9).hex()
+        == "78da2bc9c82c5600a2448592d4e2120026330516"
+    )
+    assert (
+        BinaPy("78da2bc9c82c5600a2448592d4e2120026330516")
+        .decode_from("hex")
+        .decode_from("gzip")
+        == b"this is a test"
+    )
+
+
+def test_saml() -> None:
+    assert BinaPy(
+        "fVNNj9owFLxX6n+wfCdxskAaC6go9AOJQkRoD71Urv1SLMV2aju79N/XycIqldqcLNkz82bee144puqGrlt/0Sf41YLz6Kpq7Wj/sMSt1dQwJx3VTIGjntNy/XlP04jQxhpvuKnxgDLOYM6B9dJojHbbJT4e3u+PH3eH729InpGsIuSBsJkgZJ4SnotcVHlWzdOMVZALztMpRl/BusBf4iCHUWHNoxRgD6HSEpcF8iFA0HauhZ12nmkfkCSZTkg2Sebn9IHOUjqdfcNoG5BSM9+LXbxvaBxL0URwZaqpIeJGxWV5LME+Sg5Rc2n6cn3gd1ILqX+OZ/3xDHL00/lcTIpjecZofc+/Mdq1CuxN/stp/2LC/e1BgDJJHLTg2pl4y7jDq9evEFp07aZ9VLsa4yrwTDDPOvoiHrJeZBradXC3LUwt+W/0wVjF/P/jJVHS30gxqXooBcVkvRbCgnMhZl2bp40F5sNUvG0Bx8Nat0UD0a9daIWHq0cboxpmpevmEUJw/xzzHnSI3dRhkU5QrUZ3jVPe4cJ1EY4nY0U3P+Ch8Nky7Rpj/a0f/xTvHccjlgPi/j78QKs/"
+    ).decode_from("b64").decode_from("saml") == (
+        b'<samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns'
+        b':saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="ONELOGIN_809707f0030a5d006'
+        b'20c9d9df97f627afe9dcc24" Version="2.0" ProviderName="SP test" IssueInstant="'
+        b'2014-07-16T23:52:45Z" Destination="http://idp.example.com/SSOService.php" Pr'
+        b'otocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" AssertionCons'
+        b'umerServiceURL="http://sp.example.com/demo1/index.php?acs">\r\n  <saml:Iss'
+        b"uer>http://sp.example.com/demo1/metadata.php</saml:Issuer>\r\n  <samlp:Nam"
+        b'eIDPolicy Format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress" Al'
+        b'lowCreate="true"/>\r\n  <samlp:RequestedAuthnContext Comparison="exact">\r\n'
+        b"    <saml:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Passwo"
+        b"rdProtectedTransport</saml:AuthnContextClassRef>\r\n  </samlp:RequestedAut"
+        b"hnContext>\r\n</samlp:AuthnRequest>"
+    )
