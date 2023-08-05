213b58b9f28cdaa9f05e8de46c973debb9d7e232
Guillaume Pujol <guill.p.linux@gmail.com>
2023-08-05 21:14:03 +0200

    add support for `pickle` (#5)

---
+++ binapy/parsing/pickle.py
@@ -0,0 +1,33 @@
+"""This module implements helpers for (de)serializing to/from Python `pickle` objects."""
+
+import pickle
+from typing import Any, Optional
+
+from binapy import binapy_parser, binapy_serializer
+
+
+@binapy_serializer("pickle")
+def to_pickle(
+    o: object,
+    protocol: Optional[int] = None,
+    fix_imports: bool = True,
+    buffer_callback: Any = None,
+) -> bytes:
+    """Serialize an object using `pickle.dumps()`."""
+    return pickle.dumps(
+        o, protocol=protocol, fix_imports=fix_imports, buffer_callback=buffer_callback
+    )
+
+
+@binapy_parser("pickle")
+def from_pickle(
+    bp: bytes,
+    fix_imports: bool = True,
+    encoding: str = "ASCII",
+    errors: str = "strict",
+    buffers: Any = None,
+) -> object:
+    """Deserialize an object using `pickle.loads()`."""
+    return pickle.loads(
+        bp, fix_imports=fix_imports, encoding=encoding, errors=errors, buffers=buffers
+    )
